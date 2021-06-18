03015afff5407879972f1371dc14fa692ce756bc
AmeeraX <53140185+AmeeraX@users.noreply.github.com>
2021-06-18 13:33:26 +0700

    Update and rename dot_vayu.mk to legion_vayu.mk

--- legion_vayu.mk
+++ legion_vayu.mk
@@ -11,13 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common dotOS configurations
-$(call inherit-product, vendor/dot/config/common.mk)
+# Inherit common legion configurations
+$(call inherit-product, vendor/legion/config/common.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := dot_vayu
+PRODUCT_NAME := legion_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -30,3 +30,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Inherit DotOS Official stuff.
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# LegionOS Device Maintainers
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer.name=EdvanHerv
+    
+#Gapps Architecture
+TARGET_GAPPS_ARCH := arm64
