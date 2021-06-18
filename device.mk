9b923297c94af227fc6e11254e1c8f748905095c
AmeeraX <53140185+AmeeraX@users.noreply.github.com>
2021-06-18 14:50:25 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -32,7 +32,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-dot
+    $(LOCAL_PATH)/overlay-legion
 
 # Audio
 PRODUCT_PACKAGES += \
